c6500fba9e26900e0983a8a603b2c5f8219c75aa
deri sahertian <97965918+derisamedia@users.noreply.github.com>
2023-02-10 02:20:35 +0700

    Delete netdata/execute directory

--- netdata/execute/netdata.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ubus call system board > /www/netdata/ubus/sysboard.json
-ubus call network.interface.lan status > /www/netdata/ubus/brlan.json
-ubus call network.interface.wan status > /www/netdata/ubus/eth1.json
