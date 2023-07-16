259ad025db439136dec1c509f8c3ab1fc9dc0c49
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-07-16 15:47:56 +0700

    Update alphanetdata.lua

--- alphanetdata.lua
+++ alphanetdata.lua
@@ -1,4 +1,4 @@
 module("luci.controller.alphanetdata",package.seeall)
 function index()
-entry({"admin","status","alphanetdata"},template("alphanetdata"),_("Alpha OS X NETDATA"),55).leaf=true
+entry({"admin","status","alphanetdata"},template("alphanetdata"),_("Alpha OS X NETDATA"),-1).leaf=true
 end
